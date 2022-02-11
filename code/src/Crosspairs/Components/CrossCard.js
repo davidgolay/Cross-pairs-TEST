fd5aaa6ab30df7d3e80d90e515582f00530b74ae
davidgolay <74565736+davidgolay@users.noreply.github.com>
2022-02-11 11:16:01 +0100

    Taux de change fetché et bindé dans les crosscards (UseState, UseEffect)

--- code/src/Crosspairs/Components/CrossCard.js
+++ code/src/Crosspairs/Components/CrossCard.js
@@ -3,12 +3,37 @@ import '../Styles/crosscard.css';
 
 const CrossCard = (props) => {
 
-    const endPoint = 'https://api.ibanfirst.com/PublicAPI'; //Utility for fetching url
+
+    const endPoint = 'https://api.ibanfirst.com/PublicAPI';       //Utility string for fetching url
     const [crossName, setCrossName] = useState(props.instrument); //represents the name of the cross pair (instrument:)
     const [rate, setRate] = useState(0);                 //represents the rate of the cross pair (rate:)
-    const [rateHistory, setRateHistory] = useState(1);   //represents the overtime ratio of the cross pair
-    const [overOneMonth, setOverOneMonth] = useState();
-    const [overThreeMonth, setOverThreeMonth] = useState();
+    const [overOneMonth, setOverOneMonth] = useState();           //represents the rate evolution over one month
+    const [overThreeMonth, setOverThreeMonth] = useState();       //represents the rate evolution over three month
+
+    /**
+     * This method is called to update the cross details
+     * @returns {Promise<void>}
+     */
+    const getCross = async () => {
+        await getRate()
+    }
+
+    /**
+     * This async method takes care of finding and retrieving the current rate of the cross
+     * @returns {Promise<void>}
+     */
+    const getRate = async () => {
+        const query = props.instrument;
+        const url = endPoint +'/Rate/'+ query;
+        const response = await fetch(url);
+        const responseData = await response.json();
+        setRate(responseData.rate.rate);
+        console.log(responseData);
+    }
+
+    useEffect(() => {
+        getCross();
+    }, [])
 
     return (
         <div className="CrossCard">
@@ -16,7 +41,7 @@ const CrossCard = (props) => {
                 {crossName}
             </div>
             <div className="rates-container">
-                <div className="rate-details">Actual Rate: <span>{rate}%</span></div>
+                <div className="rate-details">Actual Rate: <span>{rate}</span></div>
                 <div className="rate-details">Over one month: <span>{overOneMonth}%</span> </div>
                 <div className="rate-details">Over three months: <span>{overThreeMonth}%</span> </div>
             </div>
