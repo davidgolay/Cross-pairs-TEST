a41017aa89e5ac712a525aac545b479877a6c2bd
davidgolay <74565736+davidgolay@users.noreply.github.com>
2022-02-11 11:09:15 +0100

    ajout d'états et de binding des états avec des variables manuelement définis

--- code/src/Crosspairs/Components/CrossCard.js
+++ code/src/Crosspairs/Components/CrossCard.js
@@ -3,15 +3,22 @@ import '../Styles/crosscard.css';
 
 const CrossCard = (props) => {
 
+    const endPoint = 'https://api.ibanfirst.com/PublicAPI'; //Utility for fetching url
+    const [crossName, setCrossName] = useState(props.instrument); //represents the name of the cross pair (instrument:)
+    const [rate, setRate] = useState(0);                 //represents the rate of the cross pair (rate:)
+    const [rateHistory, setRateHistory] = useState(1);   //represents the overtime ratio of the cross pair
+    const [overOneMonth, setOverOneMonth] = useState();
+    const [overThreeMonth, setOverThreeMonth] = useState();
+
     return (
         <div className="CrossCard">
             <div className="card-title">
-                EURCHF
+                {crossName}
             </div>
             <div className="rates-container">
-                <div className="rate-details">Actual Rate: <span> 1.50236</span></div>
-                <div className="rate-details">Over one month: <span>101.05%</span> </div>
-                <div className="rate-details">Over three months: <span>103.05%</span> </div>
+                <div className="rate-details">Actual Rate: <span>{rate}%</span></div>
+                <div className="rate-details">Over one month: <span>{overOneMonth}%</span> </div>
+                <div className="rate-details">Over three months: <span>{overThreeMonth}%</span> </div>
             </div>
         </div>
     )
--- code/src/Crosspairs/Components/Crosspairs.js
+++ code/src/Crosspairs/Components/Crosspairs.js
@@ -4,6 +4,38 @@ import '../Styles/crosspairs.css';
 
 const Crosspairs = () => {
 
+    const endPoint = 'https://api.ibanfirst.com/PublicAPI';
+
+    const [crossList, setCrossList] = useState([]);
+
+    /**
+     * structure de donnée provisoire pour pouvoir générer les 'Cross Card' sans passer par l'API
+     * cette structure représente fictivement/manuellement l'attribut 'crossList' retourné par le GET sur endpoint/Cross
+     * @type {[{instrument: string, type: string},{instrument: string, type: string},{instrument: string, type: string}]}
+     */
+    const manualCrossList = [
+        { instrument: 'EURCHF', type: "Major" },
+        { instrument: 'EURGBP', type: "Major" },
+        { instrument: 'EURUSD', type: "Major" },
+    ];
+
+    const getCrossList = async () => {
+        /**
+         * Le block suivant est le vrai bloc qu'il faudrait executer pour utiliser les données fournies par l'API
+         */
+        //const endPoint = 'https://api.ibanfirst.com/PublicAPI';
+        // const url = endPoint +'/Cross';
+        // const response = await fetch(url);
+        // const responseData = await response.json();
+        // console.log(responseData.crossList);
+        // setCrossList(responseData.crossList);
+        setCrossList(manualCrossList);
+    }
+
+    useEffect(() => {
+        getCrossList();
+    }, [])
+
     return (
         <div
             className="Crosspairs">
@@ -11,10 +43,11 @@ const Crosspairs = () => {
                 <h1>Available cross pairs</h1>
                 {/* ------- AVAILABLE CROSS ------- */}
                 <div className="crosslist-container">
-                    <CrossCard />
-                    <CrossCard />
-                    <CrossCard />
-                    <CrossCard />
+                    { crossList.map((cross, index) => (
+                        <div className="cross-container" key={cross.instrument}>
+                            <CrossCard instrument={cross.instrument} />
+                        </div>
+                    ))}
                 </div>
             </div>
         </div>
